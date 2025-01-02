043db73d8c8a23100553d0ba53b194dea3cb3cfb
azizbek2510 <azizbekaliyev1011@gmail.com>
2025-01-02 12:59:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+const name = prompt("Введите имя:");
+const spaceId = prompt("Введите Space ID:");
+const spacePassword = prompt("Введите Space Password:");
+
+const userInfo = {
+    name: name,
+    ID: spaceId,
+    password: spacePassword
+};
+
+console.log(userInfo);
